883045d20c0caea1095c56864263b1b8a16e1e99
Muhammad-Ahmad-7 <2003ahmadwaheed@gmail.com>
2024-02-05 15:36:58 +0500

    Redux Configuration Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "./components";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+
 function App() {
-  return (
-    <>
-      <h1>Mega Blog Project</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(() => login({ userData }));
+        } else {
+          dispatch(logout());
+          console.log(userData);
+        }
+      })
+      .finally(() => setLoading(false));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            TODO: <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default App;
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -42,7 +42,7 @@ export class AuthService {
     try {
       return await this.account.get();
     } catch (error) {
-      console.log("Appwrite Service :: error", error);
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
     }
     return null;
   }
